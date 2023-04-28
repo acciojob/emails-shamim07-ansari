4de57a9f78ddd0a7b195b4e44979841f36d76407
Shamim <shamim73124@gmail.com>
2023-04-29 00:40:11 +0530

    Assignment completed

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,35 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password)) {
+            if(isValidPassword(newPassword)) {
+                this.password = newPassword;
+                System.out.println("Password changes successfully");
+            }
+            else System.out.println("The new password is not valid");
+        }
+        else {
+            System.out.println("Password is wrong");
+        }
+    }
+    public boolean isValidPassword(String newPassword) {
+        if(newPassword.length() < 8) return false;
+
+        boolean upperCaseLetter = false;
+        boolean lowerCaseLetter = false;
+        boolean digit = false;
+        boolean specialCharacter = false;
+
+        for(int i=0; i<newPassword.length(); i++) {
+            char ch = newPassword.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z') upperCaseLetter = true;
+            else if(ch >= 'a' && ch <= 'z') lowerCaseLetter = true;
+            else if(ch >= '0' && ch <= '9') digit = true;
+            else specialCharacter = true;
+        }
+        if(upperCaseLetter && lowerCaseLetter && digit && specialCharacter)
+            return true;
+        return false;
     }
 }
